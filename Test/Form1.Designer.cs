e75c7e9d57e18d3bdfcd3671e5df9fbcce8ef03c
kkb13 <mooremarshall13@gmail.com>
2024-04-15 18:51:04 +0800

    upload images and save on db

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_CHOOSE_IMAGE = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.create = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTN_CHOOSE_IMAGE
+            // 
+            this.BTN_CHOOSE_IMAGE.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_CHOOSE_IMAGE.Location = new System.Drawing.Point(16, 372);
+            this.BTN_CHOOSE_IMAGE.Name = "BTN_CHOOSE_IMAGE";
+            this.BTN_CHOOSE_IMAGE.Size = new System.Drawing.Size(128, 32);
+            this.BTN_CHOOSE_IMAGE.TabIndex = 1;
+            this.BTN_CHOOSE_IMAGE.Text = "Choose Image";
+            this.BTN_CHOOSE_IMAGE.UseVisualStyleBackColor = true;
+            this.BTN_CHOOSE_IMAGE.Click += new System.EventHandler(this.BTN_CHOOSE_IMAGE_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(349, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(817, 471);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 183);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(287, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // create
+            // 
+            this.create.Location = new System.Drawing.Point(188, 377);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(75, 23);
+            this.create.TabIndex = 11;
+            this.create.Text = "create";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1178, 511);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BTN_CHOOSE_IMAGE);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BTN_CHOOSE_IMAGE;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button create;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,132 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public string mySqlServerName = "sql6.freemysqlhosting.net";
+        public string mySqlServerUserId = "sql6696982";
+        public string mySqlServerPassword = "DJyeU2QQMU";
+        public string mySqlDatabaseName = "sql6696982";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("datasource=" + mySqlServerName + ";port=3306;username=" + mySqlServerUserId + ";password=" + mySqlServerPassword + ";database=" + mySqlDatabaseName + ";");
+            connection.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM sample_image", connection);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void BTN_CHOOSE_IMAGE_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Select image(*.JpG; *.png; *.Gif)|*.jpg; *.png; *.gif";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void InsertData()
+        {
+            try
+            {
+                // Check if an image is selected
+                if (!string.IsNullOrEmpty(openFileDialog1.FileName))
+                {
+                    // Read the image file into a byte array
+                    byte[] imageData;
+                    using (FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (BinaryReader br = new BinaryReader(fs))
+                        {
+                            imageData = br.ReadBytes((int)fs.Length);
+                        }
+                    }
+
+                    // Create MySQL connection and command
+                    using (MySqlConnection connection = new MySqlConnection("datasource=" + mySqlServerName + ";port=3306;username=" + mySqlServerUserId + ";password=" + mySqlServerPassword + ";database=" + mySqlDatabaseName + ";"))
+                    {
+                        MySqlCommand command = connection.CreateCommand();
+
+                        // Prepare the SQL query
+                        command.CommandText = "INSERT INTO sample_image (images) VALUES (@image)";
+                        command.Parameters.AddWithValue("@image", imageData);
+
+                        // Open the connection, execute the command
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show("Image uploaded successfully!");
+
+                    // Reload data into the DataGridView
+                    ReloadDataGridView();
+                }
+                else
+                {
+                    MessageBox.Show("Please select an image first.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ReloadDataGridView()
+        {
+            try
+            {
+                // Create a new DataTable to hold the data
+                DataTable dt = new DataTable();
+
+                // Create a new MySqlConnection
+                using (MySqlConnection connection = new MySqlConnection("datasource=" + mySqlServerName + ";port=3306;username=" + mySqlServerUserId + ";password=" + mySqlServerPassword + ";database=" + mySqlDatabaseName + ";"))
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Create a new MySqlCommand to select all data from the sample_image table
+                    using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM sample_image", connection))
+                    {
+                        // Create a new MySqlDataAdapter
+                        using (MySqlDataAdapter adp = new MySqlDataAdapter(cmd))
+                        {
+                            // Fill the DataTable with the data from the database
+                            adp.Fill(dt);
+                        }
+                    }
+                }
+
+                // Set the DataSource of the DataGridView to the new DataTable
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reloading DataGridView: " + ex.Message);
+            }
+        }
+
+
+        private void create_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Insert button clicked!");
+            InsertData();
+        }
+    }
+}
